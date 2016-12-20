0ea0de391341d8bd03c8b98f0360c35907150cf6
yuki0321 <teduka_yuki@yse-c.net>
2016-12-20 15:58:42 +0900

    FrameMan

--- 20161220/MainFrameMan2.java
+++ 20161220/MainFrameMan2.java
@@ -2,7 +2,7 @@
 	import java.awt.event.*;
 	import javax.swing.*;
 
-	public class MainFrameMan {
+	public class MainFrameMan2 {
 		public static void main(String[] args) {
 			FrameMan fm = new FrameMan();
 		}
@@ -12,9 +12,11 @@
 	class FrameMan implements ActionListener{
 		
 		JFrame frame;
-		JPanel panel;
+		JPanel panel1;
+		JPanel panel2;
 		JLabel label;
 		JButton button;
+		JTextField textfield;
 
 		public FrameMan(){
 			
@@ -23,17 +25,21 @@
 			frame = new JFrame("FrameMan");
 			frame.setBounds(100, 100, 300, 300);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setLayout( new FlowLayout() );
+			frame.setLayout( new BorderLayout() );
 
-			panel = new JPanel();
+			panel1 = new JPanel();
+			panel2 = new JPanel();
 			label = new JLabel("hello");
 			button = new JButton("follow me");
+			textfield = new JTextField(15);
 
 			button.addActionListener(this);
 		
-			panel.add(label);
-			panel.add(button);	
-			frame.add(panel);
+			panel1.add(textfield);
+			panel1.add(button);
+			panel2.add(label);	
+			frame.add(panel1, BorderLayout.NORTH );
+			frame.add(panel2, BorderLayout.CENTER);
 
 			frame.setVisible(true);
 		}
