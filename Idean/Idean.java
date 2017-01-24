6a84d4db0e4ce0fd1e5771aad29201841a577113
yuki0321 <teduka_yuki@yse-c.net>
2017-01-24 16:02:31 +0900

    2個摘出

--- Idean/Idean.java
+++ Idean/Idean.java
@@ -15,7 +15,11 @@ class IdeanMan implements ActionListener{
 	JButton btn2;
 	JPanel panel;
 	JTextArea textarea;
-
+	JScrollPane scrollPane;
+	int max = 128;
+	String[] datas = new String[max];
+	int co = 0;
+	int num = 0;	
 	
 
 	public IdeanMan(){
@@ -34,32 +38,36 @@ class IdeanMan implements ActionListener{
 
 		textarea = new JTextArea(5 , 20);
 
+
+
 		panel = new JPanel();
 		frame.add(panel);
 		panel.add(btn1);
 		panel.add(btn2);
-		panel.add(textarea);
+		//panel.add(textarea);
 
-		
+		scrollPane = new JScrollPane(textarea);
+
+		Container con = frame.getContentPane();
+        con.setLayout(new GridLayout(2,1));
+        con.add(panel);
+        con.add(scrollPane);
+
+
 
 
 		
 		frame.setVisible(true);
-	}
-	public void actionPerformed(ActionEvent ae){
-		String cmd = ae.getActionCommand();
-
-		if(cmd.equals("indean")){
-			try {
+		try {
                 // 1. ファイルを準備する
-				File inFile = new File("Idean.java");
+				File inFile = new File("data.txt");
 				FileReader fr = new FileReader(inFile);
 				BufferedReader br = new BufferedReader(fr);
                 // 2. ファイルからデータを読み取って表示する
 				String line ;
 				while ((line=br.readLine()) != null) {
-					textarea.append(line);
-					textarea.append("\n");
+					datas[co] = line;
+					co++;
 				}             
                 // 3. ファイルを閉じる
 				br.close();
@@ -67,6 +75,18 @@ class IdeanMan implements ActionListener{
 				System.out.println("ファイル読み取りエラー");
 				e.printStackTrace();
 			}
+	}
+
+	public void actionPerformed(ActionEvent ae){
+		String cmd = ae.getActionCommand();
+
+		if(cmd.equals("indean")){
+			num = (int)(Math.random()*co);
+			textarea.setText(datas[num]);
+			textarea.append("\n");
+			num = (int)(Math.random()*co);
+			textarea.append(datas[num]);
+
 		} else if(cmd.equals("idea")){
 
 			try {
@@ -85,7 +105,7 @@ class IdeanMan implements ActionListener{
                 } catch (IOException e) {
                     System.out.println("ファイル書き込みエラー");
                     e.printStackTrace();
-                }
+            }
 		}
 	}
 }
